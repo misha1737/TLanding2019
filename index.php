3e7e1698f3d63c946b9ef2c727b397efebeb8111
misha1737 <mikekozlov2@gmail.com>
2019-03-05 10:25:27 +0200

    firstComit

---
+++ index.php
@@ -0,0 +1,198 @@
+
+<!DOCTYPE html>
+
+<head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+
+
+
+<meta charset="utf-8">
+
+  <title>Депозит 2019</title>
+	
+	<meta name="robots" content="index, follow">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link href="reset.css" rel="stylesheet">
+	<link rel="stylesheet" href="stylesheets/screen.css">
+	
+	<link href="libs/jquery-ui.css" rel="stylesheet">
+	<link href="libs/unslider.css" rel="stylesheet">
+
+	<link href="libs/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="stylesheets/old.css" />
+		<link href="style.css" rel="stylesheet">
+
+
+
+</head> 
+<!-- Google Tag Manager (noscript) -->
+
+
+<body id="index"  onmouseup="CalcDecesion()">
+
+<header>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-sm-6 col-xs-12">
+			<img class="logo" src="images/logoWhite.svg" alt="">
+			<!-- <img class="cosmos" src="images/cosmos.png" alt=""> -->
+			
+			<!-- <a href="https://tas2u.tascombank.ua/"><img id="T2U" src="images/iconT2U.png" width="54px" alt=""></a> -->
+		</div>
+	
+	</div>
+</div>
+  </header>
+	
+
+<content>
+<!-- 	<div class="otstup"></div> -->
+<div class="container-fluid" id="background">
+	<div id="yolka">
+		
+		<img src="images/yolka.png" alt="">
+	</div>
+	<div class="row">
+		<section class="col-md-6 col-sm-5 deposit   about" id="aboutDepozit" data-section-name="about">
+			<div class="col-md-12 col-sm-12 textAbout">
+							<h1><span><strong>Вигравай суперставку</strong></span><br>
+				Або бонуси до депозиту</h1>
+			</div>
+				<!-- <img class="persent" src="images/richni.png" alt=""> -->
+				
+
+		</section>
+	
+		<div class="col-md-6 col-sm-7 forma " id="dzvinok">
+
+	<section class="col-md-6 col-sm-6 depositMobile   about" id="aboutDepozit " data-section-name="about">
+			<div class="col-md-12 col-sm-12">
+							<h1><span><strong>Вигравай суперставку</strong></span><br>
+				Або бонуси до депозиту</h1>
+			</div>
+				<!-- <img class="persent" src="images/richni.png" alt=""> -->
+				
+
+		</section>
+
+
+
+
+
+			<form name="contactForm" role="form" method="post" id="contactForm" action="javascript:void(null);">
+				<h2>Оформлюй зараз:</h2>
+				 <span class="placeholder" >Ім'я:</span>
+			 <input name="name" type="text" id="name" size="40" placeholder="">
+			 <br>
+			 <br>
+
+			<span class="placeholder">Телефон:</span>
+			 <input name="tel" type="text" size="40" value="380" id="tel" placeholder="Номер">
+
+		
+
+				<button id="submit"><div class="butCont">
+				<!-- <img src="images/iconButton.png" alt=""> -->
+				Надати заявку<div></button>
+					<p>  <input type="checkbox" id="Zgoda" name="subscribe" value="newsletter"> Я даю згоду на обробку персональних даних</p>
+			</form>
+		</div>
+	</div>
+	</div>
+
+	
+
+
+</content>
+
+<footer>
+	<div class="container-fluid">
+	<div class="row">
+<div class="col-sm-4 leftFooter ">
+	<img class="wLogo" src="images/logoWhite.svg">
+	<p> АТ “ТАСКОМБАНК”</p>
+<p>Ліцензія НБУ №84 від 25 жовтня 2011 р </p>
+
+
+</div>
+
+<div class="col-sm-4 centerFooter">
+
+
+	<p>
+ Лідер у номінації «Класичний депозит» у рамках рейтингу 
+ «50 провідних банків України - 2018».
+</p>
+<p>
+ АТ «ТАСКОМБАНК» посідає 3 позицію в Рейтингу надійності банківських депозитів, складеному РА «Стандарт Рейтинг» за результатами 1-го кварталу 2018 року.	
+
+</p>
+
+<p>
+АТ «ТАСКОМБАНК» є учасником Фонду гарантування вкладів фізичних осіб, який з 17.09.2012 збільшив розмір гарантованої суми кожному вкладнику банку до 200 тис. грн, включно з сумою відсотків. Свідоцтво №28 від 18.10.2012 р.	
+
+</p>
+
+
+</div>
+	
+
+<div class="col-sm-4 rightFooter">
+	<img class="phone" src="images/phone.svg" alt="">
+		<div class="footerText">
+			<h6>Інформаційний центр</h6>
+			<p>
+		0 (800) 503 580</p><p>
+		(044) 393 25 90</p>
+
+		<div class="tasAdrres">
+		<p>АТ «ТАСКОМБАНК», 01032, м. Київ,</p>
+		<p> вул. С.Петлюри, 30</p>
+		<p>Email: info@tascombank.com.ua</p>
+		</div>
+
+		</div>
+		
+	</div>
+	</div>
+</div>
+</footer>
+<div class="masagecookie">
+		<div id="closeCookie">
+		X
+	</div>
+	<p class="col-sm-6 col-sm-offset-3">Використовуючи наш сайт, Ви даєте згоду на обробку файлів cookie та користувацьких даних. Якщо ви не хочете щоб дані опрацьовувались, будь ласка, закрийте сайт
+	</p>
+
+
+</div>
+  </body>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-53SKC5F"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
+	<script src="libs/jquery/jquery-2.1.4.min.js"></script>
+
+	<script src="libs/unslider.js"></script>
+	<script src="libs/jquery-ui.min.js"></script>
+
+	<script src="libs/jquery.ui.touch-punch.js" type="text/javascript"></script>
+	
+	<script src="libs/jquery.fancybox.pack.js" type="text/javascript"></script>
+	<script src="libs/jquery.maskedinput.min.js" type="text/javascript"></script>
+	<script src="libs/common.js"></script>
+<script src="libs/bootstrap/js/bootstrap.min.js"></script>
+<script src="libs/jquery.scrollify.js"></script>
+	<script src="libs/jquery.easing.min.js"></script>
+	<script src="libs/main.js"></script>
+<!-- End Google Tag Manager (noscript) -->
+<script>
+	
+$(function(){
+  //2. Получить элемент, к которому необходимо добавить маску
+  $("#tel").mask("+38(999) 999-9999");
+});
+
+</script>
---
+++ thanku.php
@@ -0,0 +1,60 @@
+
+<!DOCTYPE html>
+
+<head>
+
+
+
+<meta charset="utf-8">
+
+  <title>Депозит 2019</title>
+	
+	<meta name="robots" content="index, follow">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	
+
+
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="reset.css" rel="stylesheet">
+	<link href="libs/jquery-ui.css" rel="stylesheet">
+	
+	<link href="libs/unslider.css" rel="stylesheet">
+	<link rel="stylesheet" href="stylesheets/screen.css">
+	
+
+
+	<link href="libs/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="stylesheets/old.css" />
+		<link href="style.css" rel="stylesheet">
+
+
+	<script src="libs/jquery/jquery-2.1.4.min.js"></script>
+	<script src="libs/bootstrap/js/bootstrap.min.js"></script>
+	<script src="libs/unslider.js"></script>
+	<script src="libs/jquery-ui.min.js"></script>
+
+	<script src="libs/jquery.ui.touch-punch.js" type="text/javascript"></script>
+	
+	<script src="libs/jquery.fancybox.pack.js" type="text/javascript"></script>
+	<script src="libs/jquery.maskedinput.min.js" type="text/javascript"></script>
+	<script src="libs/common.js"></script>
+
+
+</head> 
+
+<body id="thanku">
+
+	<div class="backgroundT">
+		<a href="index.php" ><img class="logo" src="images/logoWhite.svg" alt=""></a>
+	<h1><br>Дякуємо за вашу заявку!</h1>
+	<p>Очікуйте на дзвінок</p>
+	<p>спеціаліста банку</p>
+	<p>протягом дня</p>
+	</div>
+<!-- <img src="images/window.jpg" class="img-responsive" alt=""> -->
+
+
+
+  </body>
